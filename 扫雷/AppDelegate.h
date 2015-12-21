f1818f5bbe26a8809cb610c461516100bdbebd86
国栋 <guodong@guodongdeMacBook-Air.local>
2015-12-21 19:38:24 +0800

    Initial Commit

---
+++ 扫雷/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  扫雷
+//
+//  Created by 国栋 on 15/12/21.
+//  Copyright (c) 2015年 GD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 扫雷/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  扫雷
+//
+//  Created by 国栋 on 15/12/21.
+//  Copyright (c) 2015年 GD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
